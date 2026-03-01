e8da63540d692f0e7629619ef834fd29cee443c2
TruongDuyKhai <truongduykhai1012@gmail.com>
2026-03-01 20:03:18 +0700

    .

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
 
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 const port = process.env.PORT || 3000;
 
 /**
@@ -10,21 +10,22 @@ const port = process.env.PORT || 3000;
  */
 
 let allowedOrigins = [
-  'http://localhost:3000',
-  'https://khaidev.vercel.app',
-  'https://lamdev.vercel.app',
-  'https://noflexzone.vercel.app'
-]
+    "http://localhost:3000",
+    "https://khaidev.vercel.app",
+    "https://lamdev.vercel.app",
+    "https://noflexzone.vercel.app",
+    "https://khaidev-card.vercel.app/",
+];
 
 var corsOptions = {
-  origin: function (origin, callback) {
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
+    origin: function (origin, callback) {
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
+};
 
 app.use(cors(corsOptions));
 app.use(express.json());
@@ -34,13 +35,15 @@ const { profile, avatar, banner, badge } = require("./routes/routes");
 
 // Default route
 app.get("/", (req, res) => {
-  res.send("Welcome to the API to get Discord user's profile, avatar and banner, use /api/profile/:id, /api/avatar/:id or /api/banner/:id to get the information believe");
+    res.send(
+        "Welcome to the API to get Discord user's profile, avatar and banner, use /api/profile/:id, /api/avatar/:id or /api/banner/:id to get the information believe",
+    );
 });
-app.use('/api/profile', profile);
-app.use('/api/avatar', avatar);
-app.use('/api/banner', banner);
-app.use('/api/badge', badge);
+app.use("/api/profile", profile);
+app.use("/api/avatar", avatar);
+app.use("/api/banner", banner);
+app.use("/api/badge", badge);
 
 app.listen(port, () => {
-  console.log(`Listening on Port :: ${port}`);
+    console.log(`Listening on Port :: ${port}`);
 });
